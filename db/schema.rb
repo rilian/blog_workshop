0ebd1a909e62c294c9614cd48bd2c14120616835
rilian <dmitriyis@gmail.com>
2013-10-12 13:31:51 +0300

    add Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130918163524) do
+ActiveRecord::Schema.define(version: 20131012102831) do
+
+  create_table "comments", force: true do |t|
+    t.text    "body"
+    t.integer "post_id"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
